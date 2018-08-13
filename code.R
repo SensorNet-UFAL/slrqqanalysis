26ceac10b8e90f1ef3c65a827b2ca72a6c330923
quindai <randy.quindai@gmail.com>
2018-08-13 18:30:41 -0300

    adding code

--- code.R
+++ code.R
@@ -1,3 +1,18 @@
+
+#Read bib
+library(bibliometrix)
+library(reshape2)
+D <- readFiles("savedrecs.bib")
+M <- convert2df(D, dbsource = "isi", format = "bibtex")
+
+kword <- KeywordGrowth(M, Tag = "DE", sep = ";", top = 15, cdf = TRUE)
+DF = melt(kword, id='Year')
+
+#Read text
+#credits to  Beguería S. (2015) bibliometRics
+source('bibliometRics.R')
+bib <- read.wos('savedrecs.txt')
+
 corpus35 <- Corpus(VectorSource(references))
 
 #seminal analysis
@@ -44,4 +59,22 @@ ggplot(DFk,aes(Year,value, group=variable, color=variable))+geom_point()+
    labs(color="Author Keywords")+
    scale_colour_brewer(palette = "Set1")+
    scale_x_continuous(breaks = seq(min(DF$Year), max(DF$Year), by = 2))
+
+
+#timeline keywords ggplot
+ggplot(DF_c,aes(x=Year,y=value, group=variable, shape=variable, colour=variable))+
+  geom_point()+geom_line()+ 
+  scale_shape_manual(values = 1:15)+
+  labs(color="Author Keywords")+
+  scale_x_continuous(breaks = seq(min(DF$Year), max(DF$Year), by = 5))+
+  scale_y_continuous(breaks = seq(0, max(DF$value), by=10))+
+  guides(color=guide_legend(title = "Author Keywords"), shape=FALSE)+
+  labs(y="Count", variable="Author Keywords")+
+  theme(text = element_text(size = 10))+
+  facet_grid(variable ~ .)
+  
+  
+#timeline keywords lattice
+xyplot(data = DF_c, Year ~ value | variable, groups = variable, type = c("g","l"), ylab = "Year", par.strip.text=list(cex=0.5), auto.key = list(line = TRUE, points = FALSE, columns=2))
+
 
